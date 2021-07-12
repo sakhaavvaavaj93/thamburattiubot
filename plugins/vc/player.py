1b7c6e32fed39c15e7cfb7b0e662876c79b62e7f
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-07-13 02:34:56 +0530

    Update player.py

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -211,9 +211,9 @@ async def play_track(client, m: Message):
         )
         await mp.update_start_time()
        # await m_status.delete()
-       # print(f"- START PLAYING: {playlist[0].audio.title}")
+        print(f"- START PLAYING: {playlist[0].audio.title}")
        await mp.send_playlist()
-       await m.delete(playlist)
+       await mp.delete_playlist()
     for track in playlist[:2]:
         await download_audio(track)
     if not m.audio:
